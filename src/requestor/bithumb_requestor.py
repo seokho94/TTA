98ad86f5cb96930ec09ba8ca4f4c1dbaa041836e
Seokho <happt7889@gmail.com>
2024-12-13 15:50:40 +0900

    [Add] 하루 / 주 / 월 캔들 조회 Api 추가 및 파라미터 Null 처리

--- src/requestor/bithumb_requestor.py
+++ src/requestor/bithumb_requestor.py
@@ -3,35 +3,74 @@ from libs.requestor.abstract_requestor import AbstractRequestor
 import requests
 
 class BithumbRequestor(AbstractRequestor):
-    def get(url, headers={"accept": "application/json"}, param={}):
-        return requests.get(url, params=param, headers=headers)
+    def get(url, headers={"accept": "application/json"}, params={}):
+        return requests.get(url, params=params, headers=headers)
 
-    def post(url, param={}):
-        requests.post(url, param)
+    def post(url, params={}):
+        requests.post(url, params)
 
-    def put(url, param={}):
-        requests.put(url, param)
+    def put(url, params={}):
+        requests.put(url, params)
 
-    def delete(url, param={}):
-        requests.delete(url, param)
+    def delete(url, params={}):
+        requests.delete(url, params)
 
     @staticmethod
     def get_market_codes(isDetail=False):
         url = "https://api.bithumb.com/v1/market/all"
         params = {'isDetail': isDetail}
-        return BithumbRequestor.get(url=url, param=params)
+        return BithumbRequestor.get(url=url, params=params)
     
     @staticmethod
-    def get_min_candles(market, to=NULL, count=1, unit=1):
+    def get_min_candles(market, to=None, count=1, unit=1):
         #market: String, to: String, count: Integer
-        url= f"https://api.bithumb.com/v1/candles/minutes/{unit}"
+        url = f"https://api.bithumb.com/v1/candles/minutes/{unit}"
         params = {
             "market": market,
+            "to": to,
             "count": count
         }
 
-        if(to != NULL):
-            params.to = to
+        remove_none = {k: v for k, v in params.items() if v is not None}
 
-        return BithumbRequestor.get(url=url, param=params)
+        return BithumbRequestor.get(url=url, params=remove_none)
+    
+    @staticmethod
+    def get_day_candles(market, to=None, count=1, convertingPriceUnit=None):
+        url = "https://api.bithumb.com/v1/candles/days"
+        params = {
+            "market": market,
+            "to": to,
+            "count": count,
+            "convertingPriceUnit": convertingPriceUnit
+        }
         
+        remove_none = {k: v for k, v in params.items() if v is not None}
+
+        return BithumbRequestor.get(url=url, params=remove_none)
+    
+    @staticmethod
+    def get_week_candles(market, to=None, count=1):
+        url = "https://api.bithumb.com/v1/candles/weeks"
+        params = {
+            "market": market,
+            "to": to,
+            "count": count
+        }
+
+        remove_none = {k: v for k, v in params.items() if v is not None}
+        
+        return BithumbRequestor.get(url=url, params=remove_none)
+    
+    @staticmethod
+    def get_month_candles(market, to=None, count=1):
+        url = "https://api.bithumb.com/v1/candles/months"
+        params = {
+            "market": market,
+            "to": to,
+            "count": count
+        }
+
+        remove_none = {k: v for k, v in params.items() if v is not None}
+
+        return BithumbRequestor.get(url=url, params=remove_none)
